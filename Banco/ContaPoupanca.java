206f5ffcda41317fce9afe08bf0db97d0019d9c7
ArthurHGSilva <62658243+ArthurHGSilva@users.noreply.github.com>
2021-06-01 19:53:12 -0300

    Add files via upload

---
+++ Banco/ContaPoupanca.java
@@ -0,0 +1,5 @@
+public class ContaPoupanca extends Conta{
+	public ContaPoupanca(String nomeTitular, String numeroConta, String numeroAgencia, float saldo, String estadoConta){
+		super(nomeTitular, numeroConta, numeroAgencia, saldo, estadoConta);
+	}
+}
